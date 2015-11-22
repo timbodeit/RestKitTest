54f0e530e1341215203b4da9d2d83b02fff4a6cb
Tim Bodeit <tim@bodeit.com>
2015-11-22 15:34:49 +0100

    Initial Commit

---
+++ RestKitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestKitTest
+//
+//  Created by Tim Bodeit on 22/11/15.
+//  Copyright © 2015 Tim Bodeit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
